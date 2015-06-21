ff2efdeaae4b99239a9f1bb73e43fe8896310212
matt welson <me_number3@hotmail.com>
2015-06-21 16:02:57 +1200

    Fleshed things out further

---
+++ WebsiteProto/src/WebsiteProto/wwwroot/scripts/main.js
@@ -0,0 +1,12 @@
+﻿var headerHeight = $('.header-img').height();
+
+$(window).scroll(function () {
+    var windowScroll = $(this).scrollTop();
+
+    if (windowScroll <= headerHeight) {
+        $('.header-container').css({
+            'transform': 'translate(0px,-' + windowScroll * 0.2 + '%)'
+        });
+
+    }
+});
